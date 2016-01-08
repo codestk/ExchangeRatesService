9f0843cf6d515030d8bb13912ea0d5ba67bd5dd7
momojojo <codestk@gmail.com>
2016-01-08 17:06:03 +0700

    convert

--- ExchangeRatesService/Controllers/ExchangeWebApiController.cs
+++ ExchangeRatesService/Controllers/ExchangeWebApiController.cs
@@ -15,73 +15,73 @@ namespace ExchangeRatesService.Controllers
             return "1.0.0.0";
         }
 
-        [Route("api/GetDayInMonth/{Year}/{Month}")]
+        [Route("api/GetDayInMonth/{year}/{month}")]
         [HttpGet]
-        public P_fxrates_update[] GetDayInMonth(int Year, int Month)
+        public P_fxrates_update[] GetDayInMonth(int year, int month)
         {
-            ExchangeDb Db = new ExchangeDb();
-            return Db.GetDayInMonth(Year, Month);
+            ExchangeDb db = new ExchangeDb();
+            return db.GetDayInMonth(year, month);
         }
 
         [Route("api/GetUpdateInMonth/{dd}/{mm}/{yyyy}")]
         [HttpGet]
         public P_fxrates_update[] GetUpdateInMonth(int dd, int mm, int yyyy)
         {
-            ExchangeDb Db = new ExchangeDb();
-            return Db.GetUpdateInMonth(dd, mm, yyyy);
+            ExchangeDb db = new ExchangeDb();
+            return db.GetUpdateInMonth(dd, mm, yyyy);
         }
 
         [Route("api/GetfxratesLastUpdate")]
         [HttpGet]
         public P_fxrates_update[] GetfxratesLastUpdate()
         {
-            ExchangeDb Db = new ExchangeDb();
-            return Db.GetfxratesLastUpdate();
+            ExchangeDb db = new ExchangeDb();
+            return db.GetfxratesLastUpdate();
         }
 
         [Route("api/GetLatestfxrates")]
         [HttpGet]
         public P_fxrates[] GetLatestfxrates()
         {
-            ExchangeDb Db = new ExchangeDb();
+            ExchangeDb db = new ExchangeDb();
 
-            return Db.GetLatestfxrates();
+            return db.GetLatestfxrates();
         }
 
-        [Route("api/Getfxrates/{dd}/{mm}/{yyyy}/{upd}/{Lang}")]
+        [Route("api/Getfxrates/{dd}/{mm}/{yyyy}/{upd}/{lang}")]
         [HttpGet]
-        public P_fxrates[] Getfxrates(int dd, int mm, int yyyy, int upd, string Lang)
+        public P_fxrates[] Getfxrates(int dd, int mm, int yyyy, int upd, string lang)
         {
-            ExchangeDb Db = new ExchangeDb();
+            ExchangeDb db = new ExchangeDb();
 
-            return Db.Getfxrates(dd, mm, yyyy, upd, Lang);
+            return db.Getfxrates(dd, mm, yyyy, upd, lang);
         }
 
         [IgnoreCacheOutput]
-        [Route("api/GetChartfxrates/{Fdd}/{Fmm}/{Fyyyy}/{Tdd}/{Tmm}/{Tyyyy}/{Family}/{Lang}")]
+        [Route("api/GetChartfxrates/{fdd}/{fmm}/{fyyyy}/{tdd}/{tmm}/{tyyyy}/{family}/{lang}")]
         [HttpGet]
-        public P_fxrates[] GetChartfxrates(int Fdd, int Fmm, int Fyyyy, int Tdd, int Tmm, int Tyyyy, string Family, string Lang)
+        public P_fxrates[] GetChartfxrates(int fdd, int fmm, int fyyyy, int tdd, int tmm, int tyyyy, string family, string lang)
         {
-            ExchangeDb Db = new ExchangeDb();
+            ExchangeDb db = new ExchangeDb();
 
-            return Db.GetChartfxrates(Fdd, Fmm, Fyyyy, Tdd, Tmm, Tyyyy, Family, Lang);
+            return db.GetChartfxrates(fdd, fmm, fyyyy, tdd, tmm, tyyyy, family, lang);
         }
 
         [IgnoreCacheOutput]
-        [Route("api/GetDownloadfxrates/{Fdd}/{Fmm}/{Fyyyy}/{Tdd}/{Tmm}/{Tyyyy}/{Family}/{Lang}")]
-        public P_fxrates[] GetDownloadfxrates(int Fdd, int Fmm, int Fyyyy, int Tdd, int Tmm, int Tyyyy, string Family, string Lang)
+        [Route("api/GetDownloadfxrates/{fdd}/{fmm}/{fyyyy}/{tdd}/{tmm}/{tyyyy}/{family}/{lang}")]
+        public P_fxrates[] GetDownloadfxrates(int fdd, int fmm, int fyyyy, int tdd, int tmm, int tyyyy, string family, string lang)
         {
-            ExchangeDb Db = new ExchangeDb();
+            ExchangeDb db = new ExchangeDb();
 
-            return Db.GetDownloadfxrates(Fdd, Fmm, Fyyyy, Tdd, Tmm, Tyyyy, Family, Lang);
+            return db.GetDownloadfxrates(fdd, fmm, fyyyy, tdd, tmm, tyyyy, family, lang);
         }
 
         [Route("api/Getfxfamily")]
         [HttpGet]
         public P_fxfamily[] Getfxfamily()
         {
-            ExchangeDb Db = new ExchangeDb();
-            return Db.Getfxfamily();
+            ExchangeDb db = new ExchangeDb();
+            return db.Getfxfamily();
         }
     }
 }
